09fb93a0103675989631013f26d0bb8bbbb3293f
Prince Macaldo <60081669+ceprinmac@users.noreply.github.com>
2024-09-28 04:09:45 +0800

    Create Program.cs
    Simple display of text using double quote.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems \n Remeber, we're \"accounting\" on you!");
+    }
+}
