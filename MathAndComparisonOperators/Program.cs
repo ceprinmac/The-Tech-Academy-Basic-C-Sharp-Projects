97f33e23c515ae1d2c9876955151881912e3d59a
Prince Macaldo <60081669+ceprinmac@users.noreply.github.com>
2024-10-01 01:29:41 +0800

    Add files via upload

---
+++ MathAndComparisonOperators/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int total = 5 + 10;
+//int othertotal = 12 + 22;
+//int combined = total + othertotal;
+//Console.WriteLine(combined);
+//Console.ReadLine();
+
+//int difference = 10 - 5;
+//Console.WriteLine("Ten minus Five = " + difference.ToString());
+//Console.ReadLine();
+
+//int product = 10 * 5;
+//Console.WriteLine(product);
+//Console.ReadLine();
+
+//double quotient = 100.0 / 17.0;
+//Console.WriteLine(quotient);
+//Console.ReadLine();
+
+//int remainder = 11 % 2;
+//Console.WriteLine(remainder);
+//Console.ReadLine();
+
+int roomTemperature = 70;
+int currentTemparature = 70;
+
+//bool isWarm = currentTemparature >= roomTemperature;
+bool isWarm = currentTemparature != roomTemperature;
+Console.WriteLine(isWarm);
+Console.ReadLine();
