7a83a6664392d65d03486b5d779600e9a2331112
Prince Macaldo <60081669+ceprinmac@users.noreply.github.com>
2024-10-02 04:57:35 +0800

    Add files via upload

---
+++ MainMethod/MainMethod/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class MathOperations
+    {
+        // Method to add 1 to the int variable
+        public int Addition(int a)
+        {
+            return a + 1;
+        }
+        // Accepting double variable to operate
+        public int Addition(double a)
+        {
+            return ((int)a) + 10;
+        }
+        // Accepting string variable to operate
+        public int Addition(string a)
+        {
+            return Convert.ToInt32(a) + 20;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MainMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathOperations mathOperations = new MathOperations();
+
+            // Call the method
+            int result = mathOperations.Addition(2); // passing integer variable to Addition method
+            int decimalResult = mathOperations.Addition(2.5); // passing double variable to Addition method
+            int stringResult = mathOperations.Addition("5"); // passing string variable to Addition method
+
+            Console.WriteLine("The sum of int variable: " + result); // displaying result for integer variable
+            Console.WriteLine("The sum of decimal variable: " + decimalResult); // displaying result for double variable
+            Console.WriteLine("The sum of string variable: " + stringResult); // displaying result for string variable
+
+
+        }
+    }
+}
