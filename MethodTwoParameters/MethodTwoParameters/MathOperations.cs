62a3431a7c7e0d6f7c654e43de89fe2e7380233d
Prince Macaldo <60081669+ceprinmac@users.noreply.github.com>
2024-10-02 05:19:28 +0800

    Add files via upload

---
+++ MethodTwoParameters/MethodTwoParameters/MathOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodTwoParameters
+{
+    public class MathOperations
+    {
+        public int Subtraction(int a, int b = 0) 
+        { 
+            return a - b; 
+        }
+    }
+}
---
+++ MethodTwoParameters/MethodTwoParameters/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MethodTwoParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOperations = new MathOperations();
+
+            Console.WriteLine("Enter first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("(Optional) Enter second number (press enter to skip): ");
+            string secondInput = Console.ReadLine();
+
+            int b;
+
+            // Check if the user provided a second number  
+            if (string.IsNullOrEmpty(secondInput))
+            {
+                // If no input, default to zero  
+                b = 0;
+            }
+            else
+            {
+                // Otherwise, parse the input to an integer  
+                b = int.Parse(secondInput);
+            }
+
+            int res = mathOperations.Subtraction(a, b);
+
+            Console.WriteLine("The difference is {0}", res);
+        }
+    }
+}
