3131ba3d37e7c76c73fed71592d0865d6a5ef11c
Prince Macaldo <60081669+ceprinmac@users.noreply.github.com>
2024-10-02 04:26:14 +0800

    Add files via upload

---
+++ MethodsSubmission/MethodsSubmission/MathOperations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsSubmission
+{
+    public class MathOperations
+    {
+        public int Multiplication(int num)
+        {
+            return num * 2;
+        }
+
+        public int Addition(int num)
+        {
+            return num + 10;
+        }
+
+        public int Subtraction(int num)
+        {
+            return num - 5;
+        }
+
+        
+    }
+}
---
+++ MethodsSubmission/MethodsSubmission/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MethodsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOperations = new MathOperations();
+
+            try
+            {
+                // Ask the user for input  
+                Console.WriteLine("Number: ");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                // Call the Multiplication method and display the result  
+                int resultMultiply = mathOperations.Multiplication(userInput);
+                Console.WriteLine($"Result of multiplying by 2: {resultMultiply}");
+
+                // Call the Addition method and display the result  
+                int resultAdd = mathOperations.Addition(userInput);
+                Console.WriteLine($"Result of adding 10: {resultAdd}");
+
+                // Call the Subtraction method and display the result  
+                int resultSubtract = mathOperations.Subtraction(userInput);
+                Console.WriteLine($"Result of subtracting 5: {resultSubtract}");
+
+            }
+            catch (Exception ex)
+            {
+                // Print error message
+                Console.WriteLine("Please enter a number to perform math operations on");
+            }
+
+        }
+
+    }
+}
