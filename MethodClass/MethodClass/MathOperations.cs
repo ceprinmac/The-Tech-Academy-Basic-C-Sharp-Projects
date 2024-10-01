69bb05822b340e2244d6ad5d394abfd831ed2463
Prince Macaldo <60081669+ceprinmac@users.noreply.github.com>
2024-10-02 05:28:16 +0800

    Add files via upload

---
+++ MethodClass/MethodClass/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class MathOperations
+    {
+        public void Addition(int a, int b)
+        {
+            // Perform a math operation on the first integer  
+            int res = a + 10;
+
+            // Display the second integer to the screen  
+            Console.WriteLine($"The result after adding 10 to {a} is: {res}");
+            Console.WriteLine($"The second number is: {b}");
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MethodClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class  
+            MathOperations mathOps = new MathOperations();
+
+            // Call the Addition method, passing in two numbers  
+            mathOps.Addition(5, 20);
+
+            // Call the Addition method, specifying the parameters by name  
+            mathOps.Addition(a: 10, b: 30);
+        }
+    }
+}
