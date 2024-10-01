89a7ef79a091a104d815a8abc6eb5beeed668d64
Prince Macaldo <60081669+ceprinmac@users.noreply.github.com>
2024-10-02 06:51:11 +0800

    Add files via upload

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class Employee : Person
+    {
+        // Implementation of the SayName method specific to the Employee class 
+        public override void SayName() 
+        {
+            // Display the full name of the employee  
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AbstractClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object with firstName "Sample" and lastName "Student"  
+            Employee employee = new Employee
+            {
+                firstName = "Sample",
+                lastName = "Student"
+            };
+
+            // Call the SayName method on the Employee object to display the name  
+            employee.SayName(); // This outputs: Name: Sample Student  
+        }
+    }
+}
